e186a96d8385938acc30357c192c0f6251c22983
r0senove <64626705+r0senove@users.noreply.github.com>
2021-09-26 18:24:39 +0800

    Add files via upload

---
+++ Tugas 1 Grafkom/Demo.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3, texture3;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+	void BuildColoredTable();
+	void BuildColoredPlane();
+	void BuildColoredKakiMeja();
+	void DrawColoredTable();
+	void DrawColoredPlane();
+	void DrawColoredKakiMeja();
+};
+
